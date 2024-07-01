4970eaa941a6d682cd5ee2f9b17424158098f543
unknown <xoliqberdiyevbehruz12@gmail.com>
2024-07-01 15:49:56 +0500

    feat(users) usercontactapplication add new row

--- users/models.py
+++ users/models.py
@@ -60,6 +60,7 @@ class UserContactApplication(BaseModel):
     full_name = models.CharField(max_length=255)
     phone = models.CharField(max_length=50, unique=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_contacted = models.BooleanField(default=False)
 
     def __str__(self):
         return self.full_name
